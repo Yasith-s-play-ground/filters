90ad2339ad179f8eae80e387a3165b0514730299
Yasith Ishara Perera <yasithishara@gmail.com>
2024-07-03 16:19:33 +0530

    after filter lifecycle

--- src/main/java/lk/ijse/dep12/filter/StartFilter.java
+++ src/main/java/lk/ijse/dep12/filter/StartFilter.java
@@ -1,6 +1,8 @@
 package lk.ijse.dep12.filter;
 
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.GenericFilter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
@@ -8,10 +10,52 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 /* all requests are sent through this filter */
 //@WebFilter(filterName = "start-filter", urlPatterns = "/*")
 public class StartFilter extends HttpFilter {
+
+    static {
+        System.out.println("StartFilter class object is being initialized");
+        System.out.println("Thread : " + Thread.currentThread().getName());
+    }
+
+    public StartFilter() {
+        System.out.println("StartFilter : Constructor");
+        System.out.println("Thread : " + Thread.currentThread().getName());
+    }
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        try {
+            System.out.println("init(FilterConfig)");
+            System.out.println("Thread : " + Thread.currentThread().getName());
+            try {
+                getServletContext();
+            } catch (Exception e) {
+                System.out.println("Yet to graduate Http Filter");
+                Field configInstanceVariable = GenericFilter.class.getDeclaredField("filterConfig");
+                configInstanceVariable.setAccessible(true);
+                System.out.println("config : " + configInstanceVariable.get(this));
+
+            }
+            super.init(config);
+            Field configInstanceVariable = GenericFilter.class.getDeclaredField("filterConfig");
+            configInstanceVariable.setAccessible(true);
+            System.out.println("config : " + configInstanceVariable.get(this));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init()");
+        System.out.println("ServletContext : " + getServletContext());
+        System.out.println("Thread : " + Thread.currentThread().getName());
+    }
+
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         System.out.println("Start Filter : Incoming request");
